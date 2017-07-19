fe4897a6d0d2717eaa696703f925ec01f26af480
Tuan Nguyen <TuanNA58@fsoft.com.vn>
2017-07-19 13:49:47 -0700

    new version

--- js/soundtheme-fixed.js
+++ js/soundtheme-fixed.js
@@ -9,42 +9,48 @@ window.openSideBar = function () {
 };
 
 $(document).ready(function () {
-	// SIDEBAR OPEN
-	$(".soundtheme-btn-sidebar").click(openSideBar);
+    // SIDEBAR OPEN
+    $(".soundtheme-btn-sidebar").click(openSideBar);
 
-	// SIDEBAR CLOSE
-	$(".main-content, .soundtheme-sidebar-closed").click(function () {
-		$('body').removeClass('body-over');
-		$(".soundtheme-sidebar-menu").animate({left: '-450'}, 400, 'easeInOutExpo');
-		$(".body-lock").animate({opacity: '0'}, 600, 'easeInOutExpo');
-		$('.soundtheme-body')
-		   .delay(600)
-		   .queue(function() {
-		       $(this).removeClass("body-lock");
-		       $(this).dequeue();
-		});
-		$(".soundtheme-nav-fixed-mobil").css({ top: '0'});
-		$(".soundtheme-menu-icon, .soundtheme-search-icon").animate({ opacity: '0.9'}, 600, 'easeInOutExpo');
-	});
+    // SIDEBAR CLOSE
+    $(".main-content, .sidebar-closed-btn").click(function () {
+        $('body').removeClass('body-over');
+        $(".soundtheme-sidebar-menu").animate({left: '-450'}, 400, 'easeInOutExpo');
+        $(".body-lock").animate({opacity: '0'}, 600, 'easeInOutExpo');
+        $('.soundtheme-body')
+            .delay(600)
+            .queue(function () {
+                $(this).removeClass("body-lock");
+                $(this).dequeue();
+            });
+        $(".soundtheme-nav-fixed-mobil").css({top: '0'});
+        $(".soundtheme-menu-icon, .soundtheme-search-icon").animate({opacity: '0.9'}, 600, 'easeInOutExpo');
+    });
 
-	// SEARCH OPEN
-	$(".soundtheme-btn-search").click(function () {
-	   $(".soundtheme-top-search").animate({top: '75px'}, 600, 'easeInOutExpo');
-	   $(".soundtheme-menu-icon, .soundtheme-search-icon").animate({opacity: '0'}, 600, 'easeInOutExpo');
-	});
+    // SEARCH OPEN
+    $(".soundtheme-btn-search").click(function () {
+     //   $(".soundtheme-top-search").fadeToggle();
+        $(".soundtheme-top-search").animate({top: '75px'}, 600, 'easeInOutExpo');
+        $(".soundtheme-menu-icon, .soundtheme-search-icon").animate({opacity: '0'}, 600, 'easeInOutExpo');
+    });
 
-	// SEARCH CLOSE
-	$(".main-content").click(function () {
-	   $(".soundtheme-menu-icon, .soundtheme-search-icon").animate({opacity: '0.9'}, 700, 'easeInOutExpo');
-	   $(".soundtheme-top-search").animate({top: '-75px'}, 500, 'easeInOutExpo');
-	});
+    // SEARCH CLOSE
+    $(".main-content").click(function () {
+        $(".soundtheme-menu-icon, .soundtheme-search-icon").animate({opacity: '0.9'}, 700, 'easeInOutExpo');
+        $(".soundtheme-top-search").animate({top: '-75px'}, 500, 'easeInOutExpo', function () {
+       //     $(".soundtheme-top-search").fadeToggle();
+        });
+    });
 
-	// SEARCH ENTER CLOSE
-	$('#searchform').keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-    	$(".soundtheme-menu-icon").animate({opacity: '0.9'}, 500, 'easeInOutExpo');
-	   	$(".soundtheme-top-search").animate({top: '-75px'}, 300, 'easeInOutExpo');
-    }
-	});
+    // SEARCH ENTER CLOSE
+    /*$('#searchform').keypress(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            $(".soundtheme-menu-icon").animate({opacity: '0.9'}, 500, 'easeInOutExpo');
+            $(".soundtheme-top-search").animate({top: '-75px'}, 300, 'easeInOutExpo', function () {
+                $(".soundtheme-top-search").fadeToggle();
+            });
+        }
+    });*/
+    //$(".soundtheme-top-search").fadeToggle();
 });
